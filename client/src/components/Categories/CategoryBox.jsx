688ff8d459c8e1529e416932eddf97fb12637042
Jubair <jubair.ahmed7946@gmail.com>
2023-11-13 01:34:58 +0600

    Add: query string used in room categories

--- client/src/components/Categories/CategoryBox.jsx
+++ client/src/components/Categories/CategoryBox.jsx
@@ -1,9 +1,25 @@
 import PropTypes from "prop-types";
+import {useNavigate, useSearchParams} from "react-router-dom"
+import qs from "query-string"
 const CategoryBox = ({ category }) => {
+  const [params, setParams] = useSearchParams()
+  const navigate = useNavigate()
+  const handleCategory=()=>{
+    let currentQuery = {}
+    if(params){
+      currentQuery= qs.parse(params.toString())
+      const updateQuery = {...currentQuery, category:category.label}
+      const url = qs.stringifyUrl({
+        url: "/",
+        query:updateQuery
+      })
+      navigate(url)
+    }
+  }
   return (
-    <div className="flex flex-col items-center justify-center hover:cursor-pointer">
-      <span>{<category.icon className="text-slate-400" size={40} />}</span>
-      <p className="font-bold text-slate-800">{category.label}</p>
+    <div onClick={handleCategory} className="flex flex-col gap-2 border-b-2 pb-2 items-center justify-center hover:cursor-pointer">
+      <span>{<category.icon size={26} />}</span>
+      <p className="font-bold">{category.label}</p>
     </div>
   );
 };
