9907213768cef81827145819eb2579794335976f
Jubair <jubair.ahmed7946@gmail.com>
2023-11-22 18:56:49 +0600

    Add: host can delete listed room

--- client/src/components/Dashboard/TableRows/RoomDataRow.jsx
+++ client/src/components/Dashboard/TableRows/RoomDataRow.jsx
@@ -1,7 +1,50 @@
 /* eslint-disable react/prop-types */
 import { format } from 'date-fns'
-
+import toast from "react-hot-toast"
+import Swal from 'sweetalert2'
+import axiosSecure from '../../../api/axiosSecure';
 const RoomDataRow = ({ room }) => {
+ 
+  const handleDelete = _id=>{
+   
+      try{
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Yes, delete it!",
+          cancelButtonText: "No, cancel!",
+          reverseButtons: true
+        }).then((result) => {
+          if (result.isConfirmed) {
+            
+            axiosSecure.delete(`/room/${_id}`)
+            .then(res=>{
+              if(res.data.deletedCount>0){
+                Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success"
+            });
+              }
+            })
+          } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+          ) {
+            Swal.fire({
+              title: "Cancelled",
+              text: "Your imaginary file is safe :)",
+              icon: "error"
+            });
+          }
+        });
+      }
+      catch(err){
+        toast.error(err.message)
+      }
+  }
   return (
     <tr>
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
@@ -37,7 +80,7 @@ const RoomDataRow = ({ room }) => {
         </p>
       </td>
       <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <span className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
+        <span onClick={()=> handleDelete(room?._id)} className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
           <span
             aria-hidden='true'
             className='absolute inset-0 bg-red-200 opacity-50 rounded-full'
--- server/index.js
+++ server/index.js
@@ -135,6 +135,14 @@ async function run() {
       res.send(result);
     });
 
+    // Delete; a single room
+    app.delete("/room/:id", async(req,res)=>{
+      const id = req?.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await roomsCollection.deleteOne(query)
+      res.send(result) 
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
